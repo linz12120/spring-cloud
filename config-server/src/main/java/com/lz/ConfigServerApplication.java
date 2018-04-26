176390a950f3f0b9d3c5d442dd98475fbfa14d12
linz12120 <583120896@qq.com>
2018-04-26 20:14:39 +0800

    修改

---
+++ config-server/src/main/java/com/lz/ConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
