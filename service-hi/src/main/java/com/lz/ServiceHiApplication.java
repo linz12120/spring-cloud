1bdbb10334a266293fcb4e20d2eb490aa6f84e3d
linz12120 <583120896@qq.com>
2018-04-28 10:50:49 +0800

    添加链路追踪

--- service-hi/src/main/java/com/lz/ServiceHiApplication.java
+++ service-hi/src/main/java/com/lz/ServiceHiApplication.java
@@ -1,17 +1,27 @@
 package com.lz;
 
+import brave.sampler.Sampler;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class ServiceHiApplication {
+
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
@@ -19,8 +29,34 @@ public class ServiceHiApplication {
     @Value("${server.port}")
     String port;
 
-    @RequestMapping("/hi")
-    public String home(@RequestParam String name){
+    //@RequestMapping("/hi")
+    /*public String home(@RequestParam String name){
         return "hi "+name+",i am from port:" +port;
+    }*/
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
